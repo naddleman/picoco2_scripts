4008c4eb40ed26b536319f3e91ab97c063c054ea
naddleman <nik.addleman@gmail.com>
2020-08-23 13:44:11 -0700

    Update graph.py to handle new timestamp format

--- graph.py
+++ graph.py
@@ -2,10 +2,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.dates as mdates
-from pytz import timezone
 import sys
 
-TIMEZONE = 'US/Pacific'
 TITLE = 'Air quality: Temperature (C), Relative Humidity, CO2 (PPM)'
 
 def parseargs(args):
@@ -22,13 +20,12 @@ def parse_csv(csv):
 def produce_plots(df, title):
     fig, axs = plt.subplots(3, 1, sharex=True)
     for i, col in enumerate(df.columns[1:4], -1):
-        axs[i].plot_date(df['dtnum'], df[col], xdate=True, tz=TIMEZONE, fmt='-')
+        axs[i].plot_date(df['dtnum'], df[col], xdate=True, fmt='-')
         axs[i].set_ylabel(col)
     fig.suptitle(title)
     fig.autofmt_xdate()
     fig.set_size_inches(10, 8)
     myFmt = mdates.DateFormatter('%H:%M')
-    myFmt.set_tzinfo(timezone(TIMEZONE))
     axs[2].xaxis.set_major_formatter(myFmt)
     return fig, axs
 
